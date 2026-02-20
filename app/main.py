ccdceca93e70aa98d43817302b0aa5749d625e77
Paul Momo <mpny98@yahoo.com>
2026-02-20 15:40:30 -0500

    Test1 CI auto deploy

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "CI/CD DEPLOY SUCCESS"}
+    return {"message": "CI/CD DEPLOY SUCCESS1"}
 
 @app.get("/health")
 def health_check():
